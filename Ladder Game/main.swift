529d741374d61a907b920812210cdb6b38c87e82
Wondermini <wondermini@gmail.com>
2021-11-04 22:01:18 +0900

    프로젝트 생성 및 입력 메소드 작성

--- Ladder Game/main.swift
+++ Ladder Game/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+struct LadderGame{
+    func inputNumberOfPerson()-> Int{
+        print("인원을 입력해주세요.: ")
+        let numberOfPersons = Int(readLine() ?? "")
+        print("사다리게임 참여인원은 \(String(describing: numberOfPersons))명 입니다.")
+        return numberOfPersons!
+    }
+}
 
+var ladderGame = LadderGame()
+
+print(ladderGame.inputNumberOfPerson())
