ce296a9c9dc6340df73e0dc0d3299d52cce047de
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-14 23:54:46 +0900

    주석문 수정

--- Week2Day7/Week2Day7/68644.swift
+++ Week2Day7/Week2Day7/68644.swift
@@ -5,6 +5,11 @@
 //  Created by JUNGMIN KIM on 2021/11/14.
 //
 
+
+//인덱스 번호 바로 이후의 모든 인덱스들과 더하기를 반복한다. 제일 마지막 인덱스는 모든 조합이 반복되는 이유로 제외함.
+//전체 숫자 갯수 보다 하나 작게 루프가 생성됨.
+//중복되는 덧셈값을 제거하기 위해서 Set을 사용함.
+//Array로 변환 후 sorted로 정렬해서 반환.
 import Foundation
 
 func solution68644(_ numbers: [Int]) -> [Int] {
