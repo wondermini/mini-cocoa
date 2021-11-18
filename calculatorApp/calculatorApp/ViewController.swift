1266b844e89a7b4c5479f57d30f8185a4b6d6a99
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-19 01:16:30 +0900

    11월 18일 작업

---
+++ calculatorApp/calculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by JUNGMIN KIM on 2021/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week3day4/week3day4/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  week3day4
+//
+//  Created by JUNGMIN KIM on 2021/11/18.
+//
+
+import Foundation
+
