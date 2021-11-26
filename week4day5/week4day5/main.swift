9c91c011901df2d737d60f67f63c1c848ed40a32
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-26 17:06:49 +0900

    https://leetcode.com/problems/reverse-integer/  Reverse Integer 문제풀이

---
+++ week4day5/week4day5/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  week4day5
+//
+//  Created by JUNGMIN KIM on 2021/11/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+var solution = Solution()
+if solution.reverse(123) == 321 {
+    print("reverse test Success")
+} else {
+    print("reverse test Fail")
+}
+if solution.reverse(-123) == -321 {
+    print("reverse test Success")
+} else {
+    print("reverse test Fail")
+}
+if solution.reverse(120) == 21 {
+    print("reverse test Success")
+} else {
+    print("reverse test fail")
+}
+if solution.reverse(0) == 0{
+    print("reverse test Success")
+} else {
+    print("reverse test Success")
+}
---
+++ week4day5/week4day5/pow.swift
@@ -0,0 +1,8 @@
+//
+//  pow.swift
+//  week4day5
+//
+//  Created by JUNGMIN KIM on 2021/11/26.
+//
+
+import Foundation
