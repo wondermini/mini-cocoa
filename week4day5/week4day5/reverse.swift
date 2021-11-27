0788a4d1ac56a4765e07208833a9c36fa978bfe7
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-28 00:23:02 +0900

    fix

---
+++ week4day5/week4day5/reverse.swift
@@ -0,0 +1,29 @@
+//
+//  reverseInteger.swift
+//  week4day5
+//
+//  Created by JUNGMIN KIM on 2021/11/28.
+//
+
+import Foundation
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        if x == 0 {
+            return x
+        }
+        let str = String(x)
+
+        var tmp = 0
+        if x < 0 {
+            let srt2 = str.dropFirst()
+            let rev = String(srt2.reversed())
+            tmp = Int(rev)!
+            tmp *= -1
+        } else {
+            let revStr = String(str.reversed())
+            tmp = Int(revStr)!
+        }
+        return tmp
+    }
+}
