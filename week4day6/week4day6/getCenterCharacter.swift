0e3d6875c3686b66ae765382c9d0cbb06f55253e
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-11-27 22:24:32 +0900

    가운데 글자 가져오기 문제풀이

---
+++ week4day6/week4day6/getCenterCharacter.swift
@@ -0,0 +1,21 @@
+//
+//  getCenterCharacter.swift
+//  week4day6
+//
+//  Created by JUNGMIN KIM on 2021/11/27.
+//
+
+import Foundation
+
+func getCenterCharacter(_ s: String) -> String {
+    let tmpWordArr = Array(s)
+    let endIndex = tmpWordArr.endIndex - 1
+    var halfIndexValue = endIndex / 2
+    if endIndex % 2 == 0 {
+        return String(tmpWordArr[halfIndexValue])
+    } else {
+        var roundHalfIndexValue = Int(round(Double(halfIndexValue)))
+        return String(tmpWordArr[roundHalfIndexValue]) + String(tmpWordArr[roundHalfIndexValue+1])
+    }
+
+}
---
+++ week4day6/week4day6/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  week4day6
+//
+//  Created by JUNGMIN KIM on 2021/11/27.
+//
+
+import Foundation
+
+if getCenterCharacter("abcde") == "c" {
+    print("test success")
+} else {
+    print("test fail")
+}
+
+if getCenterCharacter("qwer") == "we" {
+    print("test success")
+} else {
+    print("test fail")
+}
