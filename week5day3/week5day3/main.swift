1dc78bf6f7fd321aa8cf4001d46547d2dbb40357
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-02 13:56:05 +0900

    IDvalidator 아이디규칙검사 문제풀이 중

--- week5day3/week5day3/main.swift
+++ week5day3/week5day3/main.swift
@@ -4,7 +4,7 @@
 //
 //  Created by JUNGMIN KIM on 2021/12/01.
 //
-
+//유클리드 호제법
 import Foundation
 
 func gcd(_ a: Int, _ b: Int) -> Int {
@@ -17,4 +17,16 @@ func gcd(_ a: Int, _ b: Int) -> Int {
 
 print(gcd(2,6))
 print(getTotalX(a: [2,6], b: [24,36]))
-print(getTotalX(a: [2,6], b: [24,36]))
+
+
+var validator = IDvalidation()
+
+//var password = readLine()!
+//var result = validator.IDValidator(password: password)
+
+//print("password=\(password) check result:\(result)")
+
+
+print(validator.letterNumDashCheck(password: "1w-2368er"))
+print(validator.letterNumDashCheck(password: "112344"))
+print(validator.letterNumDashCheck(password: "888**"))
