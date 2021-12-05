466f6c3759108c85285b4b37c02e61a04b4202aa
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-05 13:11:14 +0900

    https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true 코드 수정

--- 211204/211204/kittyAndKatty.swift
+++ 211204/211204/kittyAndKatty.swift
@@ -7,27 +7,24 @@
 
 import Foundation
 
-
 func breakingRecords(scores: [Int]) -> [Int] {
     // Write your code here
     var minCount = 0
     var maxCount = 0
-    var minimum = 0
-    var maximum = 0
-    for score in scores {
-        if minimum == 0 {
+
+    var minimum = scores[0]
+    var maximum = scores[0]
+    let tempScores = scores.dropFirst()
+    for score in tempScores {
+        // 맨 첫번째 값만 거치게됨. 첫번째를 호출하는용. 첫번째 값을 채워주기위함.
+
+        if score < minimum {
+            minCount += 1
             minimum = score
+        } else if score > maximum {
+            maxCount += 1
             maximum = score
-        }else{
-            if score < minimum {
-                minCount += 1
-                minimum = score
-            }else if score > maximum {
-                maxCount += 1
-                maximum = score
-            }
         }
     }
-    return [maxCount,minCount]
-
+    return [maxCount, minCount]
 }
--- 211204/211204/main.swift
+++ 211204/211204/main.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 
-if breakingRecords(scores: [10,5,20,20,4,5,2,25,1]) == [2,4] {
+//if breakingRecords(scores: [10,5,20,20,4,5,2,25,1]) == [2,4] {
+//    print("breakingRecords test is success")
+//}else{
+//    print("breakingRecords test is fail")
+//}
+
+
+if breakingRecords(scores: [0,9,3,10,2,20]) == [3,0] {
     print("breakingRecords test is success")
 }else{
     print("breakingRecords test is fail")
