a15c2fbe7191b0044fb8e7ba42460e60aa3a1668
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-14 23:49:01 +0900

    https://www.hackerrank.com/challenges/picking-numbers/problem 문제 풀이

---
+++ 211214/211214/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  211214
+//
+//  Created by JUNGMIN KIM on 2021/12/14.
+//
+
+import Foundation
+
+if pickingNumbers(a: [4,6,5,3,3,5,1]) == 3 {
+    print("success")
+} else {
+    print("fail")
+}
+
+if pickingNumbers(a: [1,2,2,3,1,2]) == 5 {
+    print("success")
+} else {
+    print("fail")
+}
---
+++ 211214/211214/pickingNumbers.swift
@@ -0,0 +1,32 @@
+//
+//  pickingNumbers.swift
+//  211214
+//
+//  Created by JUNGMIN KIM on 2021/12/14.
+//
+
+import Foundation
+
+
+func pickingNumbers(a: [Int]) -> Int {
+    // Write your code here
+    var result = [Int]()
+    for index in a.indices {
+        var numbers = [a[index]]
+        for secondIndex in a.indices {
+            if index != secondIndex, abs(a[index] - a[secondIndex]) <= 1 {
+                var numberFits = true
+                for number in numbers {
+                    if abs(number - a[secondIndex]) > 1 {
+                        numberFits = false
+                    }
+                }
+                if numberFits {
+                    numbers.append(a[secondIndex])
+                }
+            }
+        }
+        result = numbers.count > result.count ? numbers : result
+    }
+    return result.count
+}
