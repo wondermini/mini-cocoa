f495bb6a6a4daec2c6f6c3d1900bb05c7f39b54a
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-18 23:45:39 +0900

    변수명 수정

--- 211217/211217/medianOfTwoSortedArrays.swift
+++ 211217/211217/medianOfTwoSortedArrays.swift
@@ -11,10 +11,10 @@ class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
         var result = 0.0
         var mergeNums = nums1 + nums2
-         mergeNums = mergeNums.sorted()
+        mergeNums = mergeNums.sorted()
         if mergeNums.count % 2 == 0 {
-            let half = (mergeNums.count-1)/2
-            result = Double(mergeNums[half] + mergeNums[half + 1])/2
+            let halfIndex = (mergeNums.count-1)/2
+            result = Double(mergeNums[halfIndex] + mergeNums[halfIndex + 1])/2
         } else {
             let halfIndex = (mergeNums.count-1)/2
             result = Double(mergeNums[halfIndex])
