016236fc1147abc2780735a4d3caa278ff43f59e
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-26 21:45:19 +0900

    https://leetcode.com/problems/merge-two-sorted-lists/ 테스트 코드 작성

---
+++ 211226/211226/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  211226
+//
+//  Created by JUNGMIN KIM on 2021/12/26.
+//
+
+import Foundation
+
+
+if mergeTwoLists([], []) == [] {
+   print("mergeTwoLists test is success")
+}else {
+   print("mergeTwoLists test is fail")
+}
+
+
+if mergeTwoLists([], [0]) == [0] {
+   print("mergeTwoLists test is success")
+}else {
+   print("mergeTwoLists test is fail")
+}
---
+++ 211226/211226/mergeTwoSortedLists.swift
@@ -0,0 +1,13 @@
+//
+//  mergeTwoSortedLists.swift
+//  211226
+//
+//  Created by JUNGMIN KIM on 2021/12/26.
+//
+
+import Foundation
+
+
+func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+    
+}
