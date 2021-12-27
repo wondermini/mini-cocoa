76c733f64db138cb69fc0ea87f924275281703bb
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-27 23:53:28 +0900

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/ 테스트 코드 작성

---
+++ 211227/211227/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  211227
+//
+//  Created by JUNGMIN KIM on 2021/12/27.
+//
+
+import Foundation
+
+print("Hello, World!")
+var nums = [1,1,2]
+if removeDuplicates(&nums) == 2 {
+    print("removeDuplicates test is success")
+}else {
+    print("removeDuplicates test is fail")
+}
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+if removeDuplicates(&nums) == 5 {
+    print("removeDuplicates test is success")
+}else {
+    print("removeDuplicates test is fail")
+}
---
+++ 211227/211227/removeDuplicates.swift
@@ -0,0 +1,13 @@
+//
+//  removeDuplicates.swift
+//  211227
+//
+//  Created by JUNGMIN KIM on 2021/12/27.
+//
+
+import Foundation
+
+
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+        return 0
+    }
