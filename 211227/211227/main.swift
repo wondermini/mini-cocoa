8a9b5f17a6ebc1ab2f50f5fe6fdf8b02db3cd8e0
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-28 19:17:47 +0900

    코드 리팩토링

--- 211227/211227/main.swift
+++ 211227/211227/main.swift
@@ -9,14 +9,14 @@ import Foundation
 
 print("Hello, World!")
 var nums = [1, 1, 2]
-if removeDuplicates(&nums) == 2, nums == [1, 2] {
+if removeDuplicates(&nums) == 2, nums == [1,2,2] {
     print("removeDuplicates test is success")
 } else {
     print("removeDuplicates test is fail")
 }
 
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-if removeDuplicates(&nums) == 5, nums == [0, 1, 2, 3, 4] {
+if removeDuplicates(&nums) == 5, nums == [0, 1, 2, 3, 4, 2, 2, 3, 3, 4] {
     print("removeDuplicates test is success")
 } else {
     print("removeDuplicates test is fail")
--- 211227/211227/removeDuplicates.swift
+++ 211227/211227/removeDuplicates.swift
@@ -10,20 +10,15 @@ import Foundation
 func removeDuplicates(_ nums: inout [Int]) -> Int {
     if nums.count == 0 {
         return 0
-    }else if nums.count == 1{
-        return 1
-    }else {
-        var result = 1
-        var temp = [nums[0]]
-        for index in 1 ... nums.count-1 {
-            if nums[index] > nums[index-1] {
-                result += 1
-                temp.append(nums[index])
-            }
-        }
-        print(result)
-        print(temp)
-        nums = temp
-        return result
     }
+    var index = 1
+    for i in 1 ..< nums.count {
+        if nums[i - 1] != nums[i] {
+            nums[index] = nums[i]
+            index += 1
+        }
+    }
+    print(nums)
+    print(index)
+    return index
 }
