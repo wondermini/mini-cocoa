37bb5b091259466f882ef2d4cc3e3e95e8955962
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2021-12-31 15:40:32 +0900

    https://leetcode.com/problems/maximum-subarray/ 문제풀이

--- 211230/211230/maxSubArray.swift
+++ 211230/211230/maxSubArray.swift
@@ -7,8 +7,23 @@
 
 import Foundation
 
-
 func maxSubArray(_ nums: [Int]) -> Int {
-    
-    return 0
+    var result = Int.min
+    if nums.count == 0 {
+        result = 0
+    } else if nums.count == 1 {
+        result = nums[0]
+    } else {
+        for index in 0..<nums.count {
+            var tmpSum = 0
+            for i in index..<nums.count {
+                tmpSum += nums[i]
+                if result < tmpSum {
+                    result = tmpSum
+                }
+            }
+        }
+    }
+    print(result)
+    return result
 }
