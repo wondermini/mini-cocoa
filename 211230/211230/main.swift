24a54d7a682adb80b914fb1ba2e4ec64618a972a
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-02 23:19:18 +0900

    SwiftUI프로젝트 생성

--- 211230/211230/main.swift
+++ 211230/211230/main.swift
@@ -9,60 +9,58 @@ import Foundation
 
 print(Int.min)
 
-if searchInsert([1,3,5,6], 5) == 2 {
+if searchInsert([1, 3, 5, 6], 5) == 2 {
     print("searchInsert test is success")
-}else {
+} else {
     print("searchInsert test is fail")
 }
 
-
-if searchInsert([1,3,5,6], 2) == 1 {
+if searchInsert([1, 3, 5, 6], 2) == 1 {
     print("searchInsert test is success")
-}else {
+} else {
     print("searchInsert test is fail")
 }
 
-
-if searchInsert([1,3,5,6], 7) == 4 {
+if searchInsert([1, 3, 5, 6], 7) == 4 {
     print("searchInsert test is success")
-}else {
+} else {
     print("searchInsert test is fail")
 }
 
-//maxSubArray
-//if maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6 {
+// maxSubArray
+// if maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6 {
 //    print("maxSubArray test is success")
-//}else {
+// }else {
 //    print("maxSubArray test is fail")
-//}
+// }
 //
-//if maxSubArray([1]) == 1 {
+// if maxSubArray([1]) == 1 {
 //    print("maxSubArray test is success")
-//}else {
+// }else {
 //    print("maxSubArray test is fail")
-//}
+// }
 
-if maxSubArray([5,4,-1,7,8]) == 23 {
+if maxSubArray([5, 4, -1, 7, 8]) == 23 {
     print("maxSubArray test is success")
-}else {
+} else {
     print("maxSubArray test is fail")
 }
 
-//lengthOfLastWord
+// lengthOfLastWord
 if lengthOfLastWord("Hello World") == 5 {
     print("lengthOfLastWord test is success")
-}else {
+} else {
     print("lengthOfLastWord test is fail")
 }
 
 if lengthOfLastWord("fly me   to   the moon ") == 4 {
     print("lengthOfLastWord test is success")
-}else {
+} else {
     print("lengthOfLastWord test is fail")
 }
 
 if lengthOfLastWord("luffy is still joyboy") == 6 {
     print("lengthOfLastWord test is success")
-}else {
+} else {
     print("lengthOfLastWord test is fail")
 }
---
+++ SwiftUI_tutotial/SwiftUI_tutotial/SwiftUI_tutotialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutotialApp.swift
+//  SwiftUI_tutotial
+//
+//  Created by JUNGMIN KIM on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutotialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
