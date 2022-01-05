465cc09ba4ef4701be0566e336f8a400ee9e0671
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-05 15:34:19 +0900

    https://leetcode.com/problems/add-binary/ 테스트 코드 작성

---
+++ 220105/220105/addBinary.swift
@@ -0,0 +1,12 @@
+//
+//  addBinary.swift
+//  220105
+//
+//  Created by JUNGMIN KIM on 2022/01/05.
+//
+
+import Foundation
+
+func addBinary(_ a: String, _ b: String) -> String {
+    return "0"
+}
---
+++ 220105/220105/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  220105
+//
+//  Created by JUNGMIN KIM on 2022/01/05.
+//
+
+import Foundation
+
+if addBinary("11", "1") == "100" {
+    print("addBinary test is success")
+}else {
+    print("addBinary test is fail")
+}
+
+if addBinary("1010", "1011") == "10101" {
+    print("addBinary test is success")
+}else {
+    print("addBinary test is fail")
+}
