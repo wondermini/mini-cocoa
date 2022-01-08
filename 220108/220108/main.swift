dc45b56a0d2ab57ddc861483ce84e1140052a7df
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-08 23:19:09 +0900

    https://leetcode.com/problems/climbing-stairs/ 테스트 코드 작성

---
+++ 220108/220108/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  220108
+//
+//  Created by JUNGMIN KIM on 2022/01/08.
+//
+
+import Foundation
+
+
+if climbStairs(2) == 2 {
+    print("climbStairs test is success")
+}else {
+    print("climbStairs test is fail")
+}
+
+
+
+if climbStairs(3) == 3 {
+    print("climbStairs test is success")
+}else {
+    print("climbStairs test is fail")
+}
