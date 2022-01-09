f8cff920061484594d15d54984d52e0018fec40c
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-09 23:54:15 +0900

    https://leetcode.com/problems/climbing-stairs/ 문제풀이

---
+++ 220108/220108/climbStairs.swift
@@ -0,0 +1,25 @@
+//
+//  climbStairs.swift
+//  220108
+//
+//  Created by JUNGMIN KIM on 2022/01/08.
+//
+
+import Foundation
+
+
+func climbStairs(_ n: Int) -> Int {
+    if n < 4 {
+        return n
+    }
+    var prevPrev = 2
+    var prev = 3
+    var count = 4
+    while count != n {
+        var newPrev = prevPrev + prev
+        prevPrev = prev
+        prev = newPrev
+        count += 1
+    }
+    return prevPrev + prev
+}
