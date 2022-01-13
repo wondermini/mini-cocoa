0650612df4c48b7098ce7c0f8c5df90aee4a5585
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-13 23:33:45 +0900

    버그 수정

--- 220113/220113/mergeSortedArray.swift
+++ 220113/220113/mergeSortedArray.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    nums1 = (nums1 + nums2).sorted().filter { $0 > 0 }
+    nums1 = Array(nums1[0..<m])
+    nums1 = (nums1 + nums2).sorted()
 }
