4c088fd5bb1d2b557690cea3e4e1cf4f39d84a17
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-16 23:36:26 +0900

    https://leetcode.com/problems/pascals-triangle/discuss/272580/Swift 문제풀이

--- 220115/220115/pascalsTriangle.swift
+++ 220115/220115/pascalsTriangle.swift
@@ -8,6 +8,25 @@
 import Foundation
 
 func generate(_ numRows: Int) -> [[Int]] {
-     
-    return [[0]]
- }
+        if numRows == 0 {
+            return [[Int]]()
+        }
+        if numRows == 1{
+            return [[1]]
+        }
+        if numRows == 2{
+            return [[1],[1,1]]
+        }
+        var ans = [[1],[1,1]]
+        for row in 2..<numRows{
+            var newRow = Array(repeating: 1, count: row + 1)
+            var i = 1
+            while i < row{
+                newRow[i] = ans[row-1][i-1] + ans[row-1][i]
+                i += 1
+            }
+            ans.append(newRow)
+        }
+        return ans
+    }
+ 
