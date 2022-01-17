339cd4380ee40b56112bbc0ad0f1d1b14bb78895
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-17 23:20:49 +0900

    https://leetcode.com/problems/pascals-triangle-ii/ 테스트 코드 작성

---
+++ 220117/220117/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  220117
+//
+//  Created by JUNGMIN KIM on 2022/01/17.
+//
+
+import Foundation
+
+if getRow(3) == [1,3,3,1] {
+    print("getRow test is success")
+}else {
+    print("getRow test is fail")
+}
+
+
+if getRow(0) == [1] {
+    print("getRow test is success")
+}else {
+    print("getRow test is fail")
+}
+
+
+if getRow(1) == [1,1] {
+    print("getRow test is success")
+}else {
+    print("getRow test is fail")
+}
