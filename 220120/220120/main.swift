c568b83a2a14391693a34261fc871541c7563e27
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-01-22 23:43:03 +0900

    단위 변환기 문제풀이

--- 220120/220120/main.swift
+++ 220120/220120/main.swift
@@ -10,4 +10,8 @@ import Foundation
 let converter = UnitConverter()
 
 print(converter.converter(value: "18cm inch"))
-
+print(converter.converter(value: "25.4inch m"))
+print(converter.converter(value: "0.5m inch"))
+print(converter.converter(value: "183cm"))
+print(converter.converter(value: "3.14m"))
+print(converter.converter(value: "2.54inch"))
--- unitConverter.swift
+++ unitConverter.swift
@@ -10,29 +10,52 @@ import Foundation
 struct UnitConverter {
     func converter(value: String) -> String {
         var tmpValue = value
-        
+
         if tmpValue.contains("cm inch") {
             tmpValue.removeLast(7)
             let inch = cmToInch(value: Double(tmpValue)!)
             return String(format: "%f inch", inch)
 
-        } else if tmpValue.contains("") {
-                    
+        } else if tmpValue.contains("inch m") {
+            tmpValue.removeLast(6)
+            let cm = inchToCm(value: Double(tmpValue)!)
+            let m = cmToM(value: cm)
+            return String(format: "%f m", m)
+        } else if tmpValue.contains("m inch") {
+            tmpValue.removeLast(6)
+            let cm = mToCm(value: Double(tmpValue)!)
+            let inch = cmToInch(value: cm)
+            return String(format: "%f inch", inch)
+        } else if tmpValue.contains("cm") {
+            tmpValue.removeLast(2)
+            let m = cmToM(value: Double(tmpValue)!)
+            return String(format: "%f m", m)
+        } else if tmpValue.contains("m") {
+            tmpValue.removeLast(1)
+            let cm = mToCm(value: Double(tmpValue)!)
+            return String(format: "%f cm", cm)
+        } else if tmpValue.contains("inch") {
+            tmpValue.removeLast(4)
+            let cm = inchToCm(value: Double(tmpValue)!)
+            return String(format: "%f cm", cm)
         }
-     
+
         return ""
     }
+
     func cmToInch(value: Double) -> Double {
         return value/2.54
     }
-//    func inchToCm{
-//
-//    }
-//    func cmToM{
-//
-//    }
-//    func mToCm{
-//
-//    }
-    
+
+    func inchToCm(value: Double) -> Double {
+        return value*2.54
+    }
+
+    func cmToM(value: Double) -> Double {
+        return value/100
+    }
+
+    func mToCm(value: Double) -> Double {
+        return value*100
+    }
 }
