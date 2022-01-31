f3646d648648c3f1bffbedadc8cb8893938fd4d3
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-01 00:23:44 +0900

    printLeftTree 문제풀이

---
+++ printLeftTree.swift
@@ -0,0 +1,18 @@
+//
+//  printLeftTree.swift
+//  220131
+//
+//  Created by JUNGMIN KIM on 2022/01/31.
+//
+
+import Foundation
+
+
+func printLeftTree(lines: Int){
+    for i in 0..<lines{
+        for j in 0...i{
+            print("🍎", terminator: " ")
+        }
+        print("")
+    }
+}
