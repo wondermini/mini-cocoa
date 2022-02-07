47572cd5b8b14dfe2402133c4a3fd18ad772e5e0
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-07 22:06:03 +0900

    printDiamond 문제풀이

--- 220131/printDiamond.swift
+++ 220131/printDiamond.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-func printDiamond(lines: Int){
-    
+func printDiamond(lines: Int) {
+    for i in 1...lines {
+        for _ in stride(from: lines, to: i, by: -1) {
+            print(terminator: " ")
+        }
+        for j in 1...i {
+            print("*", terminator: " ")
+        }
+        print(" ")
+    }
+    for i in stride(from: lines, to: 0, by: -1) {
+        for _ in stride(from: lines, to: i-1, by: -1) {
+            print(terminator: " ")
+        }
+        for j in stride(from: 1, to: i, by: 1) {
+            print("*", terminator: " ")
+        }
+        print(" ")
+    }
 }
