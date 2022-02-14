43899519c9668e4838e86a321e7eacff6b646f0e
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-14 15:11:05 +0900

    추가미션으로 풀어본 미션6. printSquare() 문제풀이

---
+++ 220131/additonalMission6.swift
@@ -0,0 +1,33 @@
+//
+//  additonalMission6.swift
+//  220131
+//
+//  Created by JUNGMIN KIM on 2022/02/14.
+//
+
+import Foundation
+
+func square(lines:Int) -> [[String]]{
+    var num = 1
+    var result = [[String]]()
+    for _ in 1...lines{
+        result.append([String]())
+        for _ in 1...lines{
+            result[result.endIndex-1].append(String(num))
+            num += 1
+            if num <= 10{
+                result[result.endIndex-1].append(String(""))
+            }
+        }
+    }
+    return result
+}
+
+func rePrintSquare(arrs:[[String]]){
+    for row in arrs{
+        for value in row{
+            print(value, terminator: " ")
+        }
+        print()
+    }
+}
