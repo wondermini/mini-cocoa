086fe620b11974a96610114637a0882d41c7a445
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-15 20:50:49 +0900

    추가문제 문제풀이

---
+++ 220131/additionalMission8.swift
@@ -0,0 +1,16 @@
+//
+//  additionalMission8.swift
+//  220131
+//
+//  Created by JUNGMIN KIM on 2022/02/14.
+//
+
+import Foundation
+
+//func diamond(lines: Int){
+//    var result = [[String]]()
+//    for i in 1...lines{
+//        result.append([String]())
+//        for 
+//    }
+//}
--- 220131/printDiamond.swift
+++ 220131/printDiamond.swift
@@ -8,8 +8,17 @@
 import Foundation
 
 func printDiamond(lines: Int) {
-    for i in 1...lines {
-        for _ in stride(from: lines, to: i, by: -1) {
+    var halfup = 0
+    var halfdown = 0
+    if lines % 2 == 0 {
+        halfup = lines / 2
+        halfdown = lines / 2 + 1
+    } else {
+        halfup = lines / 2 + 1
+        halfdown = lines / 2 + 1
+    }
+    for i in 1...halfup {
+        for _ in stride(from: halfup, to: i, by: -1) {
             print(terminator: " ")
         }
         for _ in 1...i {
@@ -20,8 +29,8 @@ func printDiamond(lines: Int) {
     
     print("-------------")
     
-    for i in stride(from: lines, to: 0, by: -1) {
-        for _ in stride(from: lines, to: i-1, by: -1) {
+    for i in stride(from: halfdown, to: 0, by: -1) {
+        for _ in stride(from: halfdown, to: i-1, by: -1) {
             print(terminator: " ")
         }
         for _ in stride(from: 1, to: i, by: 1) {
