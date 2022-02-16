4b228a261fc444bfc0bda33c960acbaf61904deb
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-16 16:42:50 +0900

    사다리게임 인원입력

---
+++ 220215/220215/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  220215
+//
+//  Created by JUNGMIN KIM on 2022/02/15.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ ladderGame.swift
@@ -0,0 +1,18 @@
+//
+//  ladderGame.swift
+//  220215
+//
+//  Created by JUNGMIN KIM on 2022/02/15.
+//
+
+import Foundation
+
+struct LadderGame {
+    func drawLadder() {
+        let participants = Int(readLine()!)!
+        if participants < 2 || participants > 8 {
+            print("Input Error")
+            return;
+        } else {}
+    }
+}
