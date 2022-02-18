a0a0caa07abf482e7d07be22f759e39d0fc0f3f0
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-02-18 20:43:19 +0900

    알고리즘 1000 문제풀이

---
+++ 222018/1000.swift
@@ -0,0 +1,13 @@
+//
+//  1000.swift
+//  222018
+//
+//  Created by JUNGMIN KIM on 2022/02/18.
+//
+
+import Foundation
+
+func plus(A: Int, B: Int){
+let result = A+B
+print(result)
+}
---
+++ 222018/222018/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  222018
+//
+//  Created by JUNGMIN KIM on 2022/02/18.
+//
+
+import Foundation
+
+plus(A: 1, B: 2)
