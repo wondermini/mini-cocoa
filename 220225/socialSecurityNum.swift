73271a56837e46f4b9ebada5af14873b4397ccfd
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-03-07 23:34:58 +0900

    주민번호 판별검사 문제풀이 중

---
+++ 220225/socialSecurityNum.swift
@@ -0,0 +1,15 @@
+//
+//  socialSecurityNum.swift
+//  220225
+//
+//  Created by JUNGMIN KIM on 2022/03/07.
+//
+
+import Foundation
+
+struct socialSecurityNum{
+    func checksocialSecurityNum(inputNums: String){
+        let inputNums = readLine()!
+        
+    }
+}
