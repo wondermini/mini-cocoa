c21099439815ab99eb09393dd64bfd3c58b51932
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-03-18 21:50:32 +0900

    단위변환 문제풀이

---
+++ measurementApp/measurementApp/measurement.swift
@@ -0,0 +1,44 @@
+//
+//  measurement.swift
+//  measurementApp
+//
+//  Created by JUNGMIN KIM on 2022/03/18.
+//
+
+import Foundation
+
+enum Unit {
+    case inch
+    case cm
+    case m
+    
+    var description: String {
+        switch self {
+        case .inch: return "inch"
+        case .cm: return "cm"
+        case .m: return "m"
+        }
+    }
+
+    struct Measurement {
+        var value: Int
+        var unit: Unit
+    }
+
+    struct MeasureApp {
+        func ConvertMeasurement(inputValue: Double, fromUnit: Unit, toUnit: Unit) -> String {
+            var tmpValue = 0.0
+            if fromUnit == Unit.inch, toUnit == Unit.cm {
+                tmpValue += Double(inputValue * 2.54)
+                return String(tmpValue) + toUnit.description
+            } else if fromUnit == Unit.cm, toUnit == Unit.inch {
+                tmpValue += Double(inputValue / 2.54)
+                return String(tmpValue) + toUnit.description
+//            } else if fromUnit == Unit.m, toUnit == Unit.inch {
+            }else{
+                tmpValue += Double(inputValue * 39.37)
+                return String(tmpValue) + toUnit.description
+            }
+        }
+    }
+}
