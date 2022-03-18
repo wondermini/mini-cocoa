c4cbf82321c4c85142d31e35bd771231ccc95284
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-03-18 22:36:27 +0900

    measure 앱 문제풀이

--- measurementApp/measurementApp/ViewController.swift
+++ measurementApp/measurementApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func convertButton(_ sender: Any) {
+        var result = ""
+        let app = MeasureApp()
+        if inputTextField.text != "" {
+            let array = inputTextField.text?.split(separator: " ")
+            result = app.convertMeasurement(inputValue: Double(array![0])!,
+                                            fromUnit: Unit.init(rawValue: String(array![1]))!,
+                                            toUnit: Unit.init(rawValue: String(array![2]))!)
+            
+            self.printLabel.text = result
+        }
+    }
+    
 
 }
 
--- measurementApp/measurementApp/measurement.swift
+++ measurementApp/measurementApp/measurement.swift
@@ -7,38 +7,38 @@
 
 import Foundation
 
-enum Unit {
-    case inch
-    case cm
-    case m
-    
-    var description: String {
-        switch self {
-        case .inch: return "inch"
-        case .cm: return "cm"
-        case .m: return "m"
-        }
-    }
+enum Unit: String {
+    case inch = "inch"
+    case cm = "cm"
+    case m = "m"
 
-    struct Measurement {
-        var value: Int
-        var unit: Unit
-    }
+//    var description: String {
+//        switch self {
+//        case .inch: return "inch"
+//        case .cm: return "cm"
+//        case .m: return "m"
+//        }
+//    }
+}
 
-    struct MeasureApp {
-        func ConvertMeasurement(inputValue: Double, fromUnit: Unit, toUnit: Unit) -> String {
-            var tmpValue = 0.0
-            if fromUnit == Unit.inch, toUnit == Unit.cm {
-                tmpValue += Double(inputValue * 2.54)
-                return String(tmpValue) + toUnit.description
-            } else if fromUnit == Unit.cm, toUnit == Unit.inch {
-                tmpValue += Double(inputValue / 2.54)
-                return String(tmpValue) + toUnit.description
+struct Measurement {
+    var value: Int
+    var unit: Unit
+}
+
+struct MeasureApp {
+    func convertMeasurement(inputValue: Double, fromUnit: Unit, toUnit: Unit) -> String {
+        var tmpValue = 0.0
+        if fromUnit == Unit.inch, toUnit == Unit.cm {
+            tmpValue += Double(inputValue * 2.54)
+            return String(tmpValue) + toUnit.rawValue
+        } else if fromUnit == Unit.cm, toUnit == Unit.inch {
+            tmpValue += Double(inputValue / 2.54)
+            return String(tmpValue) + toUnit.rawValue
 //            } else if fromUnit == Unit.m, toUnit == Unit.inch {
-            }else{
-                tmpValue += Double(inputValue * 39.37)
-                return String(tmpValue) + toUnit.description
-            }
+        } else {
+            tmpValue += Double(inputValue * 39.37)
+            return String(tmpValue) + toUnit.rawValue
         }
     }
 }
