5882bd243733159495226a7cc85b1ead6f121c42
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-03-25 23:20:22 +0900

    singleNumber 문제풀이

--- Single Number/220324/singleNumber.swift
+++ Single Number/220324/singleNumber.swift
@@ -7,15 +7,25 @@
 
 import Foundation
 
+func singleNumber(_ nums: [Int]) -> Int {
+//        var removedArr = [Int]()
+//        for i in 0..<nums.count{
+//            for j in i..<nums.count{
+//                if i == j {
+//                    nums.remove(at:i)
+//                }
+//            }
+//        }
 
-    func singleNumber(_ nums: [Int]) -> Int {
-        var result = Int[]
-        for i in 0..<nums.count{
-            for j in i..<nums.count{
-                if i == j {
-                    nums.remove(at:i)
-                }
-            }
+    var elementCount = [Int: Int]()
+
+    for i in nums {
+        if elementCount[i] != nil {
+            elementCount[i]! += 1
+        } else {
+            elementCount[i] = 1
         }
-        return removedArr[0]
     }
+
+    return elementCount.filter { $0.value == 1 }.keys.first!
+}
