abc5e39e7feddb748fe133c248e240ba7bcefe0a
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-03-27 00:10:27 +0900

    passwordInputApp 문제풀이 중

--- PasswordInputApp/PasswordInputApp/ViewController.swift
+++ PasswordInputApp/PasswordInputApp/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var levelOfPassword: UILabel!
+    @IBOutlet var inputpassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func confirm(_ sender: Any) {
+        //inputpassword.text는 입력창에 넣은 값을 말한다. 없을때는 zerolengthString이다.
+        let passwordLevel = passwordValidator(password: inputpassword.text ?? "")
+        self.levelOfPassword.text = String(format: "%1d Level", passwordLevel)
+    }
 }
-
---
+++ PasswordInputApp/PasswordInputApp/passwordInput.swift
@@ -0,0 +1,14 @@
+//
+//  passwordInput.swift
+//  PasswordInputApp
+//
+//  Created by JUNGMIN KIM on 2022/03/26.
+//
+
+import Foundation
+
+
+func passwordValidator(password: String) -> Int {
+    
+    return 0
+}
