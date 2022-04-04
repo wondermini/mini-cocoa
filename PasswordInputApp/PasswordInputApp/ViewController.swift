a8b7e45c1a225d8b76cd672962e14c4c337eca60
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-04 23:43:46 +0900

    연습문제3-비밀번호입력앱 문제풀이

--- PasswordInputApp/PasswordInputApp/ViewController.swift
+++ PasswordInputApp/PasswordInputApp/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var levelColor: UILabel!
     @IBOutlet var levelOfPassword: UILabel!
     @IBOutlet var inputpassword: UITextField!
     override func viewDidLoad() {
@@ -25,15 +26,44 @@ class ViewController: UIViewController {
             return 1
         }else if passwordArr.self.allSatisfy({$0.isLetter}) == true && password.count<8{
              return 2
-        }else if passwordChar.isSubset(of: lettersLower) && passwordChar.isSubset(of: lettersUpper) && passwordChar.isSubset(of: nums) && password.count>=8{
+        }else if passwordChar.isSubset(of: lettersLower) && passwordChar.isSubset(of: lettersUpper)  && passwordChar.isSubset(of: nums) && password.count>=8 {
             return 4
-        }else if passwordChar.
+        }else if passwordChar.isSubset(of: lettersLower) && passwordChar.isSubset(of: lettersUpper) && passwordChar.isSubset(of: symbol) && password.count>=8 {
+            return 5
         }else{
             return 3
         }
     }
 
     @IBAction func confirm(_ sender: Any) {
-
+        let passwordLevel = passwordValidator(password: inputpassword.text ?? "")
+        var passwordLevelMsg = ""
+        
+        switch  passwordLevel{
+        case 1:
+            levelColor.backgroundColor = UIColor.red
+            passwordLevelMsg = "아주 약한 암호"
+        case 2:
+            levelColor.backgroundColor = UIColor.orange
+            passwordLevelMsg = "약한 암호"
+        case 4:
+            levelColor.backgroundColor = UIColor.blue
+            passwordLevelMsg = "보통 암호"
+        case 5:
+            levelColor.backgroundColor = UIColor.green
+            passwordLevelMsg = "강한 암호"
+        default:
+            levelColor.backgroundColor = UIColor.yellow
+            passwordLevelMsg = "아주 강한 암호"
+            
+        }
+        
+        levelOfPassword.text = passwordLevelMsg
+        
+       
+        
+        
+        
     }
+    
 }
