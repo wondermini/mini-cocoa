dbf843093edee660b335e93f81359fc812e9e1e7
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-06 00:59:23 +0900

    단어맞추기앱 문제풀이 중

--- matchWords/matchWords/ViewController.swift
+++ matchWords/matchWords/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var wordTextField: UITextField!
+    @IBOutlet var secondInputWord: UILabel!
+    @IBOutlet var firstInputWord: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func confirmButton(_ sender: Any) {
+        if secondInputWord.text == "" || secondInputWord.text == nil {
+            wordTextField.placeholder = "입력해주세요"
+        }
+        wordTextField.textAlignment = NSTextAlignment.center
+    }
 }
-
