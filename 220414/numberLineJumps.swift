659e1d42a8d136289f40979482cc1ed16a2f98e7
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-16 23:57:02 +0900

    https://www.hackerrank.com/challenges/kangaroo/problem

---
+++ 220414/numberLineJumps.swift
@@ -0,0 +1,14 @@
+//
+//  numberLineJumps.swift
+//  220414
+//
+//  Created by JUNGMIN KIM on 2022/04/16.
+//
+
+import Foundation
+
+
+func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
+    // Write your code here
+
+}
