80406853a5d184d24d6d07e90510c64a9df6f56a
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-20 12:03:53 +0900

    https://www.hackerrank.com/challenges/kangaroo/problem 문제풀이

--- 220414/numberLineJumps.swift
+++ 220414/numberLineJumps.swift
@@ -7,8 +7,22 @@
 
 import Foundation
 
-
 func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
+    var kangaroo1 = x1
+    var kangaroo2 = x2
+    repeat {
+        kangaroo1 += v1
+        kangaroo2 += v2
 
+        if kangaroo1 == kangaroo2 {
+            return "YES"
+        }
+        if x1 < x2, kangaroo1 > kangaroo2 {
+            return "NO"
+        }
 
+        if x1 > x2, kangaroo1 < kangaroo2 {
+            return "NO"
+        }
+    } while true
 }
