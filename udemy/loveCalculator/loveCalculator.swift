3a3b770abc0e9e9ddc4c8519e0ee7b9e1604462a
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-26 15:01:33 +0900

    loveCalculator  코드 수정

--- udemy/loveCalculator/loveCalculator.swift
+++ udemy/loveCalculator/loveCalculator.swift
@@ -10,8 +10,10 @@ import Foundation
 func loveCalculator() {
     let loveScore = Int.random(in: 0 ... 100)
 
-    if loveScore == 100 {
+    if loveScore > 80 {
         print("Your love each other like Kanye loves Kanye")
+    }else if 40 < loveScore || loveScore < 80{
+        print("You go together like Coke and Mentos")
     } else {
         print("You'll be forever alone")
     }
